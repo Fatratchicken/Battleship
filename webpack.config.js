d8ca36edb75499185305afad4e2022e1c2576230
moshe <moshebekritsky@gmail.com>
2026-01-14 17:33:35 +0200

    Initial Commit, Boilerplate Environment
    - webpack
    - eslint
    - prettier
    - babel / jest
    
    custom scripts can be run - check the
    "package.json" file for scripts

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: '/src/index.js',
+	
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/Battleship/',
+		clean: true
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader'
+			}
+		]
+	},
+	
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+	],
+
+	mode: 'development'
+};
