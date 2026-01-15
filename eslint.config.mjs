c75d3d3d2b1c28555747e6e66db6f8fd7489d7e0
moshe <moshebekritsky@gmail.com>
2026-01-15 11:04:15 +0200

    Bug fix: Fixed EsLint compatibility with Jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,6 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+
+  {files: ["**/*.test.js", "**/*.spec.js"], languageOptions: {globals: {...globals.jest}}}
 ]);
