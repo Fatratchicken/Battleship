ba645d2a3b321c74894b1b7200cdaf5c0e3ad3f9
moshe <moshebekritsky@gmail.com>
2026-01-20 22:41:50 +0200

    created gameboard file and Gameboard class.
    includes tests in gameboard.test.js file.
    
    - placeShip function for ship placement
    - receiveAttack function for index bombing
    - allSunk function to check if all boats allSunk
    
    Gameboard attributes:
    
    - ships to track all boats in an array
    - board - matrix with board data

---
+++ src/gameboard.js
@@ -0,0 +1,71 @@
+import { Ship } from "./ship"
+
+class Gameboard{
+    constructor(){
+        this.width = 10;
+        this.height = 10;
+
+        this.ships = [5,4,3,3,2].map(length => new Ship(length));
+        this.shipCount = this.ships.length;
+
+        this.board = Array.from(Array(this.height), () => Array(this.width).fill(null));
+    }
+
+    placeShip(ship, x, y, vertical){
+        x = Math.abs(x);
+        y = Math.abs(y);
+
+        const length = ship.length;
+
+        if (x >  this.width - 1  || y > this.height - 1){
+            throw new Error('index out of board range');
+        }
+
+        if (x + length > this.width && !vertical || y + length > this.height && vertical){
+            throw new Error('length out of range');
+        }
+
+        let indexes = [];
+        let startIndex = (vertical) ? y : x;
+
+        for (let index = startIndex, i = 0; i < length; i++, index++){
+            const currentIndex = (vertical) ? [index, x] : [y, index];
+
+            if (this.board[currentIndex[0]][currentIndex[1]] !== null) throw new Error('ship overlap');
+            
+            indexes.push(currentIndex);
+        }
+
+        indexes.forEach(([y,x]) => this.board[y][x] = ship);
+    }
+
+    receiveAttack(x,y){
+        x = Math.abs(x);
+        y = Math.abs(y);
+
+        if (x > this.width - 1 || y > this.height - 1){
+            throw new Error('index out of board range');
+        }
+
+        const indexValue = this.board[y][x];
+
+        if (indexValue === 'X' || indexValue === 'O') throw new Error('index previously chosen');
+
+        else if (typeof indexValue === 'object' && indexValue !== null){
+            indexValue.hit();
+
+            if(indexValue.isSunk()) this.shipCount--;
+
+            this.board[y][x] = 'X';
+        }
+        
+        else this.board[y][x] = 'O';
+    }
+
+    allSunk(){
+        return (this.shipCount > 0) ? false : true;
+    }
+}
+
+export {Gameboard}
+
---
+++ tests/gameboard.test.js
@@ -0,0 +1,311 @@
+import { Gameboard } from "../src/gameboard";
+import { Ship } from "../src/ship";
+
+describe('Class Gameboard', () => {
+    test('Is Defined', () => {
+        expect(Gameboard).toBeDefined();
+    })
+
+    describe('Board Attributes', () => {
+        describe('Gameboard.board', () => {
+            const output = [
+                [null, null, null, null, null, null, null, null, null, null], 
+                [null, null, null, null, null, null, null, null, null, null], 
+                [null, null, null, null, null, null, null, null, null, null], 
+                [null, null, null, null, null, null, null, null, null, null], 
+                [null, null, null, null, null, null, null, null, null, null], 
+                [null, null, null, null, null, null, null, null, null, null], 
+                [null, null, null, null, null, null, null, null, null, null], 
+                [null, null, null, null, null, null, null, null, null, null], 
+                [null, null, null, null, null, null, null, null, null, null], 
+                [null, null, null, null, null, null, null, null, null, null], 
+
+            ]
+            test(`board => ${JSON.stringify(output)}`, () => {
+                expect(new Gameboard().board).toEqual(output);
+            })
+        })
+
+        describe('Gameboard.ships', () => {
+            test('constructor () => [ship, ship ...]', () => {
+                expect(new Gameboard().ships).toEqual([
+                    new Ship(5), new Ship(4), new Ship(3),
+                    new Ship(3), new Ship(2)
+                ])
+            })
+        })
+    })
+
+    describe('placeShip function', () => {
+        describe('placement errors', () => {
+            let gameboard = new Gameboard();
+
+            const indexOutOfRangeTests = [
+                {x: 10, y: 10},
+                {x: 10, y: 9},
+                {x: 9, y: 10},
+                {x: -10, y: -10}
+            ]
+
+            indexOutOfRangeTests.forEach(({x, y}) => {
+                test(`placeShip (Ship, ${x}, ${y}) => Error (index out of board range)`, () => {
+                    expect(() => gameboard.placeShip(new Ship(), x, y)).toThrow('index out of board range');
+                })
+            })
+
+            const lengthOutOfRangeTest = [
+                {ship: new Ship(2), x: 9, y: 0, vertical: false},
+                {ship: new Ship(2), x: 0, y: 9, vertical: true} 
+            ]
+
+            lengthOutOfRangeTest.forEach(({ship, x, y, vertical}) => {
+                test(`placeShip (len = ${ship.length}, ${x}, ${y}) => Error (length out of range)`, () => {
+                    expect(() => gameboard.placeShip(ship, x, y, vertical)).toThrow('length out of range');            
+
+                })
+            })
+
+           const shipOverlapTests = [
+                {ship: new Ship(1), x: 0, y: 0, vertical: false, board: [
+                    [new Ship(1), null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                ]},
+
+                {ship: new Ship(5), x: 1, y: 0, vertical: true, board: [
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, new Ship(2), new Ship(2), null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                ]}
+            ]
+
+            shipOverlapTests.forEach(({ship, x, y, vertical, board}) => {
+                const testboard = new Gameboard();
+                testboard.board = board;
+
+                test(`placeShip (${x}, ${y}), board [${board}] => Error (ship overlap)`, () => {
+                    expect(() => testboard.placeShip(ship, x, y, vertical)).toThrow('ship overlap');
+                })
+            })
+        })
+
+
+        describe('placeShip placement', () => {
+            const tests = [
+                {ship: new Ship(1), x: 0, y: 0, vertical: false, output: [
+                    [new Ship(1), null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null]
+                ]},
+
+                {ship: new Ship(1), x: 9, y: 9, vertical: false, output: [
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, new Ship(1)]
+                ]},
+
+                {ship: new Ship(2), x: 8, y: 9, vertical: false, output: [
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, new Ship(2), new Ship(2)], 
+                ]},
+
+
+                {ship: new Ship(2), x: 9, y: 8, vertical: true, output: [
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, new Ship(2)], 
+                    [null, null, null, null, null, null, null, null, null, new Ship(2)], 
+                ]}
+            ]
+
+            tests.forEach(({ship, x, y, vertical, output}) => {
+                test(`placeShip (${ship.length}, ${x}, ${y}, ${vertical}) => ${output}`, () => {
+                    const clearBoard = new Gameboard();
+                    clearBoard.placeShip(ship, x, y, vertical);
+
+                    expect(clearBoard.board).toEqual(output);
+                })
+            })
+        })
+    })
+
+
+    describe('receiveAttack function', () => {
+        const basicBoard = new Gameboard();
+
+        describe('index errors', () => {
+            const chosenTests = [
+                {x: 0, y: 0, board: [
+                    ['O', null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                ]},
+
+                {x: 2, y: 3, board: [
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, 'X', null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                    [null, null, null, null, null, null, null, null, null, null], 
+                ]}
+            ]
+
+            chosenTests.forEach(({x, y, board}) => {
+                test(`receiveAttack (${x}, ${y}, [${board}]) => Error (index previously chosen)`, () => {
+                    const testBoard = new Gameboard();
+                    testBoard.board = board
+
+                    expect(() => testBoard.receiveAttack(x,y)).toThrow('index previously chosen');
+                })
+            })
+
+            const indexTests = [
+                {x: 10, y: 10},
+                {x: -10, y: -10}
+            ]
+
+            indexTests.forEach(({x,y}) => {
+                test(`receiveAttack (${x}, ${y}) => Error (index out of board range)`, () => {
+                    expect(() => basicBoard.receiveAttack(x,y)).toThrow('index out of board range')
+                })
+            })
+        })
+
+        describe('receiveAttack (x,y) => object', () => {
+            const objectTests = [
+                {x: 0, y: 0, board: [
+                    [new Ship(1), null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null]
+                ],
+            
+                output: [
+                    ['X', null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                ]}
+            ]
+
+            objectTests.forEach(({x, y, board, output}) => {
+                test(`(${x}, ${y}), board: ${board}, output: ${output}`, () => {
+                    const testBoard = new Gameboard();
+                    testBoard.board = board;
+
+                    testBoard.receiveAttack(x,y);
+
+                    expect(testBoard.board).toEqual(output);
+                })
+            })
+        })
+
+        describe('receiveAttack (x,y) => null', () => {
+            const nullTests = [
+                {x: 0, y: 0, board: [
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null]
+                ],
+
+                output: [
+                    ['O', null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null, null, null]
+                ]}
+            ]
+
+            nullTests.forEach(({x, y, board, output}) => {
+                test(`(${x}, ${y}), board: ${board}, output: ${output}`, () => {  
+                    const testBoard = new Gameboard();
+                    testBoard.board = board;
+
+                    testBoard.receiveAttack(x,y);
+
+                    expect(testBoard.board).toEqual(output);
+                })
+            })
+        })
+        
+    })
+})
