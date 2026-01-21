8a54b19b8311843fce5e92b5ee3b19390e29d7ac
moshe <moshebekritsky@gmail.com>
2026-01-21 16:28:56 +0200

    Game flow PseudoCode in index.js module.
    - removed the console.log()
    -inserted PseudoCode game flow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./style.css";
 
-console.log("Up and Running");
+// create players
+
+// place ships - function in seperate module.
+
+// start a loop until someone wins / loses
+// current player chooses a coordinate
+// display board (both current player and other player, other player board without boats) - function in seperate module
+// check if hit, check wins - function in seperate bodule.
+// swap player
+
+// display winner
